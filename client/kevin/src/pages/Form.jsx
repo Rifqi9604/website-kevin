b633cbfd68d0a5d5da884a1bab9573f3bddaf687
Rifqi9604 <88605839+Rifqi9604@users.noreply.github.com>
2024-02-05 19:34:59 +0700

    Update Form.jsx

--- client/kevin/src/pages/Form.jsx
+++ client/kevin/src/pages/Form.jsx
@@ -16,7 +16,7 @@ function Form() {
       message: formik.values.message,
     };
 
-    fetch("http://localhost:5000/connect", {
+    fetch("/connect", {
       method: "post",
       headers: {
         "Content-type": "application/json",
